c25ff84d581c607d5b16dbf85db5c316a7cc2efb
gigi2391 <ivanov.gabriel@lttdeva.ro>
2025-08-15 16:47:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~2cUjhSIC#-WdMEzXg5VchdMrfl12CakPe8btHntQ0_3o-rIalUoU",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~eFVkSLSZ#AhxVZ3zTMTABFgqctIjOXo_L2rwP9AKEjmFazTi59p0",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
